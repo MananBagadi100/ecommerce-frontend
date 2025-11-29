ddfd0b1ea6080716c5a4eaef6179920326c351d7
MananBagadi100 <mananbagadi100@gmail.com>
2025-11-29 16:01:16 +0530

    Added top categories section structure and css

--- src/components/HomeSections/TopCategoriesSection.jsx
+++ src/components/HomeSections/TopCategoriesSection.jsx
@@ -1,7 +1,16 @@
 import '../../styles/HomeStyles/TopCategoriesSectionStyles.css'
+import Sunglasses from './../../assets/sunglassesT.png'
 const TopCategoriesSection = () => {
     return (
-        <div>hello</div>
+        <div className='top-category-full-container'>
+            <div className="top-category-title">Top Categories</div>
+            <div className="top-category-content-area">
+                <div className="top-category-card">
+                    <img src={Sunglasses} alt='Image not found'className="top-category-image"/>
+                    <div className="top-category-image-label">Try Sunglasses</div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default TopCategoriesSection
