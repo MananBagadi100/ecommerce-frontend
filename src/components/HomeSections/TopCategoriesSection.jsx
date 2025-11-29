7fa3e5ebf167bf4cc088255a5b08f8a33f9a7ca2
MananBagadi100 <mananbagadi100@gmail.com>
2025-11-29 16:37:47 +0530

    Added top categories section properly to prevent overflows and consistent UI

--- src/components/HomeSections/TopCategoriesSection.jsx
+++ src/components/HomeSections/TopCategoriesSection.jsx
@@ -1,13 +1,23 @@
 import '../../styles/HomeStyles/TopCategoriesSectionStyles.css'
 import Sunglasses from './../../assets/sunglassesT.png'
+import Laptops from './../../assets/Laptops.png'
+import Perfumes from './../../assets/Perfumes.png'
 const TopCategoriesSection = () => {
     return (
         <div className='top-category-full-container'>
             <div className="top-category-title">Top Categories</div>
             <div className="top-category-content-area">
                 <div className="top-category-card">
+                    <img src={Perfumes} alt='Image not found'className="top-category-image"/>
+                    <div className="top-category-image-label">Explore Perfumes</div>
+                </div>
+                <div className="top-category-card">
                     <img src={Sunglasses} alt='Image not found'className="top-category-image"/>
                     <div className="top-category-image-label">Try Sunglasses</div>
+                </div>
+                <div className="top-category-card">
+                    <img src={Laptops} alt='Image not found'className="top-category-image"/>
+                    <div className="top-category-image-label">Shop Laptops</div>
                 </div>
             </div>
         </div>
