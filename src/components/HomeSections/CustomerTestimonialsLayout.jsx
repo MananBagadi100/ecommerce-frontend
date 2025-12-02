32e434b2e35eff0235c29906e9724bebce4ee8ad
MananBagadi100 <mananbagadi100@gmail.com>
2025-12-02 11:55:47 +0530

    Added rounding of the profile pics and also added a json file for the review (it is where the review data is fetched from )

--- src/components/HomeSections/CustomerTestimonialsLayout.jsx
+++ src/components/HomeSections/CustomerTestimonialsLayout.jsx
@@ -1,13 +1,13 @@
 import './../../styles/HomeStyles/CustomerTestimonialsLayoutStyles.css'
 import ProfilePic from './../../assets/ProfilePic1.png'
-const CustomerTestimonialsLayout = () => {
+const CustomerTestimonialsLayout = ({customer}) => {
     return (
         <div className='testimonials-layout-full-card'>
             <div className="testimonials-image-wrapper">
-                <img src={ProfilePic} alt="Image not found" className="testimonials-user-profile-pic" />
+                <img src={customer.profile_photo} alt="Image not found" className="testimonials-user-profile-pic" />
             </div>
-            <div className="testimonials-user-name-section">Boss</div>
-            <div className="testimonials-user-review-section">There was very good food and the service was very good</div>
+            <div className="testimonials-user-name-section">{customer.name}</div>
+            <div className="testimonials-user-review-section">{customer.review}</div>
         </div>
     )
 }
--- src/components/HomeSections/CustomerTestimonialsSection.jsx
+++ src/components/HomeSections/CustomerTestimonialsSection.jsx
@@ -2,23 +2,46 @@ import './../../styles/HomeStyles/CustomerTestimonialsSectionStyles.css'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import CustomerTestimonialsData from './CustomerTestimonialsData.json'
 import CustomerTestimonialsLayout from './CustomerTestimonialsLayout';
 const CustomerTestimonialsSection = () => {
     const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
+        dots: true,
+        infinite: true,
+        slidesToScroll: 1,
+        slideToShow: 3,
+        autoplay: true,
+        speed: 2500,
+        autoplaySpeed: 3000,
+        cssEase: "linear",
+        responsive : [
+            {
+                breakpoint : 734,
+                settings : {
+                    slideToShow : 1
+                }
+            },
+            {
+                breakpoint : 1024,
+                settings : {
+                    slideToShow : 2
+                }
+            }
+        ]
+    };
     return (
         <div className="customer-review-full-container">
             <div className="customer-review-title">Customer Testimonials</div>
             <div className="customer-review-content-area">
-                <Slider {...settings}>
-                    <CustomerTestimonialsLayout />
-                    <CustomerTestimonialsLayout />
-                </Slider>       
+                <div className="customer-review-slider-area">
+                    <Slider {...settings}>
+                        {
+                            CustomerTestimonialsData.map((customer) => (
+                                <CustomerTestimonialsLayout customer={customer}/>
+                            ))
+                        }
+                    </Slider>  
+                </div>      
             </div>
         </div>
     )
