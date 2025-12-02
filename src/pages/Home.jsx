3c62b85492bfe1cebb1c17475591e633cfc6b56f
MananBagadi100 <mananbagadi100@gmail.com>
2025-12-02 14:46:03 +0530

    Cleanup: remove node_modules and dist from repo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,4 @@
-import { Link } from "react-router-dom"
 import '../styles/HomeStyles.css'
-import { useEffect, useState } from "react"
-import heroImage from "../assets/heroImage.png"
-import ProfileImg1 from '../assets/ProfileImg1.png'
-import ProfileImg2 from '../assets/ProfileImg2.jpg'
-import ProfileImg3 from '../assets/ProfileImg3.jpg'
-
 import HeroSection from "../components/HomeSections/HeroSection.jsx"
 import TopCategoriesSection from "../components/HomeSections/TopCategoriesSection.jsx"
 import TopProductsSection from "../components/HomeSections/TopProductsSection.jsx"
