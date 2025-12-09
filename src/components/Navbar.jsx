8af2491aa497cc4655ffc5565a7aafd7485db749
MananBagadi100 <mananbagadi100@gmail.com>
2025-12-09 14:45:53 +0530

    just a refactor was saving code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,72 +1,69 @@
-import React from 'react'
 import '../styles/NavbarStyles.css'
-import { NavLink } from 'react-router-dom'
-import { useState, useContext, useEffect } from 'react'
-import { LoginContext } from '../context/LoginContext'
+import {NavLink} from 'react-router-dom'
+import {useState, useContext, useEffect} from 'react'
+import {LoginContext} from '../context/LoginContext'
+import { MenuOpen } from '@mui/icons-material'
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false)
-  const toggleMenu = () => setMenuOpen(!menuOpen)
+    const [menuOpen,setMenuOpen] = useState(false)
+    const toggleMenu = () => setMenuOpen(!menuOpen)
 
-  const navItems = [
-    { path: '/', label: 'Home' },
-    { path: '/products', label: 'Products' },
-    { path: '/about-us', label: 'About-us' },
-    { path: '/contact-us', label: 'Contact Us' },
-    { path: '/cart', label: 'Cart' },
-  ]
+    console.log('The width is ', window.innerWidth)
+    const navItems = [
+        {path: '/', label: 'Home' }, 
+        {path: '/products', label: 'Products' }, 
+        {path: '/about-us', label: 'About-us' }, 
+        {path: '/contact-us', label: 'Contact Us' }, 
+        {path: '/cart', label: 'Cart' }
+    ]
 
-  const [loginBtnValue, setLoginBtnValue] = useState('Login')
-  const [themeBtnTxt, setThemeBtnTxt] = useState('☀️')
-  const [theme, setTheme] = useState('light')
-  const value = useContext(LoginContext)
+    const [loginBtnValue, setLoginBtnValue] = useState('Login')
+    const [themeBtnTxt, setThemeBtnTxt] = useState('☀️') 
+    const [theme, setTheme] = useState('light') 
 
-  const toggleTheme = () => {
-    if (theme === 'light') {
-      document.body.classList.add('dark-theme')
-      setTheme('dark')
-      setThemeBtnTxt('🌙')
-    } else {
-      document.body.classList.remove('dark-theme')
-      setTheme('light')
-      setThemeBtnTxt('☀️')
+    const toggleTheme = () => {
+        if (theme === 'light') {
+            document
+                .body
+                .classList
+                .add('dark-theme')
+            setTheme('dark')
+            setThemeBtnTxt('🌙')
+        } else {
+            document
+                .body
+                .classList
+                .remove('dark-theme')
+            setTheme('light')
+            setThemeBtnTxt('☀️')
+        }
     }
-  }
 
-  useEffect(() => {
-    value.loginState === true ? setLoginBtnValue('Logged IN !') : setLoginBtnValue('Login')
-  }, [value.loginState])
+    // useEffect(() => {
+    //     value.loginState === true
+    //         ? setLoginBtnValue('Logged IN !')
+    //         : setLoginBtnValue('Login')
+    // }, [value.loginState])
 
-  return (
-    <nav className='navbar'>
-      <button className='nav-hamburger' onClick={toggleMenu} aria-label='Toggle Navigation menu'>
-        ☰
-      </button>
-
-      <ul className={`nav-list ${menuOpen ? 'open' : ''}`}>
-        {navItems.map((items) => (
-          <li key={items.path} className='nav-item'>
-            <NavLink to={items.path} onClick={() => setMenuOpen(false)}>
-              {items.label}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-
-      <ul className={`nav-btn-list ${menuOpen ? 'open' : ''}`}>
-        <li key='/login' className='nav-btn-item'>
-          <NavLink to='/login' onClick={() => setMenuOpen(false)}>
-            {loginBtnValue}
-          </NavLink>
-        </li>
-        {/* <li className='nav-btn-item-2'>
-          <button className='nav-toggle-theme-btn' onClick={toggleTheme}>
-            {themeBtnTxt}
-          </button>
-        </li> */}
-      </ul>
-    </nav>
-  )
+    return (
+        <div className="navbar-full-container">
+            <div className="navbar-content-area">
+                <div className={`navbar-item-links-${MenuOpen ? 'mobile': 'desktop'} `}>
+                    {
+                        <ul className='navbar-item-list'>
+                            {
+                                navItems.map((item) => (
+                                    <li className='navbar-item' key={item.label}>
+                                        <NavLink to={`${item.path}`}>{item.label}</NavLink>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    }
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
