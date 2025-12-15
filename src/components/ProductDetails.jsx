bf005693f1751b8d07f384621debde825c45e921
MananBagadi100 <mananbagadi100@gmail.com>
2025-12-15 12:04:25 +0530

    Fixed the reviews section (desktop view) and also fixed the layout of the image and the textual details in the product details page

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -21,7 +21,7 @@ const ProductDetails = () => {
         }
         navigate('/cart')   // Navigate to the cart immediately
     }
-    
+
     function convertFirstCharToUpperCase(str) {
         let updatedString = ''
         for (let i = 0; i < str.length; i++) {
@@ -74,99 +74,106 @@ const ProductDetails = () => {
                     <div className="product-breadcrums">
                         Home &gt; Products &gt; {convertFirstCharToUpperCase(product_details.category)}
                     </div>
-                    <div className="product-details-image-wrapper">
-                        <img src={`${product_details.images[0]}`} alt="Image not Found" className="product-details-image" />
-                    </div>
-                    <div className="product-details-information-area">
-                        <div className="product-details-main-information">
-                            <div className="product-details-info-id">
-                                Id :{product_details.id}
+                    <div className="product-full-details">
+                        <div className="product-details-image-wrapper">
+                            <img src={`${product_details.images[0]}`} alt="Image not Found" className="product-details-image" />
+                        </div>
+                        <div className="product-full-text-details">
+                            <div className="product-details-information-area">
+                                <div className="product-details-main-information">
+                                    <div className="product-details-info-id">
+                                        Id :{product_details.id}
+                                    </div>
+                                    <div className="product-details-info-title">
+                                        {product_details.title}
+                                    </div>
+                                    <div className="product-details-info-category">
+                                        <span className="product-details-info-name">Category :</span>
+                                        {convertFirstCharToUpperCase(product_details.category)}
+                                    </div>
+                                    <div className="product-details-info-price">
+                                        ${product_details.price}
+                                    </div>
+                                    <div className="product-details-info-price-support">Inclusive of all taxes
+                                    </div>
+                                </div>
+
+                                <div className="product-details-extra-info-wrapper">
+                                    <div className="product-details-extra-info-title">
+                                        More Info
+                                    </div>
+                                    <div className="product-details-info-warranty">
+                                        <span className="product-details-info-name">Warranty :</span>
+                                        {product_details.warrantyInformation}
+                                    </div>
+                                    <div className="product-details-info-shipping">
+                                        <span className="product-details-info-name">Shipping :</span>
+                                        {product_details.shippingInformation}
+                                    </div>
+                                    <div className="product-details-info-availability">
+                                        <span className="product-details-info-name">Availability :</span>
+                                        {product_details.availabilityStatus}
+                                    </div>
+                                    <div className="product-details-info-description">
+                                        <span className="product-details-info-name">Description :</span>
+                                        {product_details.description}
+                                    </div>
+                                    <div className="product-details-info-return-policy">
+                                        <span className="product-details-info-name">Return Policy :</span>
+                                        {product_details.returnPolicy}
+                                    </div>
+                                </div>
                             </div>
-                            <div className="product-details-info-title">
-                                {product_details.title}
+                            {/* Product Updator buttons */}
+                            <div className="product-details-quantity-updator-wrapper">
+                                {/* Decrement product quantity and prevent it from going to zero */}
+                                <button 
+                                    className="product-details-quantity-updator-btn"        
+                                    onClick={() => productQuantity !== 1 ?  setProductQuantity((prev) => prev - 1) : ''}   
+                                >        
+                                    -
+                                </button>
+                                <div className="product-details-quantity-selected" style={{fontSize:18}}>{productQuantity}</div>
+                                <button 
+                                    className="product-details-quantity-updator-btn" 
+                                    onClick={() => setProductQuantity((prev) => prev + 1)}>
+                                        +
+                                </button>
                             </div>
-                            <div className="product-details-info-category">
-                                <span className="product-details-info-name">Category :</span>
-                                {convertFirstCharToUpperCase(product_details.category)}
-                            </div>
-                            <div className="product-details-info-price">
-                                ${product_details.price}
-                            </div>
-                            <div className="product-details-info-price-support">Inclusive of all taxes
+                            {/* Buttons area */}
+                            <div className="product-details-info-btn-wrapper">
+                                <button 
+                                    className="product-details-info-addToCart-btn" 
+                                    onClick={() => addManyProductsToCart(product_details,productQuantity)}
+                                >      
+                                    Add To Cart
+                                </button>
+                                <button 
+                                    className="product-details-info-buyNow-btn" 
+                                    onClick={() => handleBuyNowBtn(product_details,productQuantity)}
+                                >
+                                    Buy Now
+                                </button>
                             </div>
                         </div>
-
-                        <div className="product-details-extra-info-wrapper">
-                            <div className="product-details-extra-info-title">
-                                More Info
-                            </div>
-                            <div className="product-details-info-warranty">
-                                <span className="product-details-info-name">Warranty :</span>
-                                {product_details.warrantyInformation}
-                            </div>
-                            <div className="product-details-info-shipping">
-                                <span className="product-details-info-name">Shipping :</span>
-                                {product_details.shippingInformation}
-                            </div>
-                            <div className="product-details-info-availability">
-                                <span className="product-details-info-name">Availability :</span>
-                                {product_details.availabilityStatus}
-                            </div>
-                            <div className="product-details-info-description">
-                                <span className="product-details-info-name">Description :</span>
-                                {product_details.description}
-                            </div>
-                            <div className="product-details-info-return-policy">
-                                <span className="product-details-info-name">Return Policy :</span>
-                                {product_details.returnPolicy}
-                            </div>
-                        </div>
-
-                    </div>
-                    <div className="product-details-quantity-updator-wrapper">
-                         {/* Decrement product quantity and prevent it from going to zero */}
-                        <button 
-                            className="product-details-quantity-updator-btn"        
-                            onClick={() => productQuantity !== 1 ?  setProductQuantity((prev) => prev - 1) : ''}   
-                        >        
-                            -
-                        </button>
-                        <div className="product-details-quantity-selected" style={{fontSize:18}}>{productQuantity}</div>
-                        <button 
-                            className="product-details-quantity-updator-btn" 
-                            onClick={() => setProductQuantity((prev) => prev + 1)}>
-                                +
-                        </button>
-                    </div>
-                    <div className="product-details-info-btn-wrapper">
-                        <button 
-                            className="product-details-info-addToCart-btn" 
-                            onClick={() => addManyProductsToCart(product_details,productQuantity)}
-                        >            {/* onClick={value.addManyProductsToCart( product_details ,product_quantity)}*/}
-                            Add To Cart
-                        </button>
-                        <button 
-                            className="product-details-info-buyNow-btn" 
-                            onClick={() => handleBuyNowBtn(product_details,productQuantity)}
-                        > {/* onClick={handleBuyNowBtn(product_details) */ }
-                            Buy Now
-                        </button>
                     </div>
 
                     
                     <div className="product-details-reviews-wrapper">
                         <div className="product-details-reviews-title">Reviews</div>
-                        {
-                            product_details.reviews.map((review) => (
-                                <div className="product-details-review-info" key={`${review.reviewerName}-${review.reviewerEmail}-${review.rating}-${review.comment}`}>
-                                    <div className="product-details-reviewerName">{review.reviewerName}</div>
-                                    <div className="product-details-reviewerEmail">{review.reviewerEmail}</div>
-                                    <div className="product-details-review-rating">{review.rating} ⭐️</div>
-                                    <div className="product-details-review-comment">{review.comment}</div>
-                                    <div className="product-details-date">{formatDate(review.date)}</div>
-                                </div>
-                            ))
-                        }
+                        <div className="product-details-reviews-listing-container">
+                            {
+                                product_details.reviews.map((review) => (
+                                    <div className="product-details-review-info" key={`${review.reviewerName}-${review.reviewerEmail}-${review.rating}-${review.comment}`}>
+                                        <div className="product-details-reviewerName">{review.reviewerName}</div>
+                                        <div className="product-details-reviewerEmail">{review.reviewerEmail}</div>
+                                        <div className="product-details-review-rating">{review.rating} ⭐️</div>
+                                        <div className="product-details-review-comment">{review.comment}</div>
+                                        <div className="product-details-review-date">{formatDate(review.date)}</div>
+                                    </div>
+                                ))
+                            }
+                        </div>
                     </div>
                 </div>
 
